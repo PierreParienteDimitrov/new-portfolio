1cabf9e8a8fe1c170a0c32a3dac1c23ab6e857f4
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2023-11-26 20:35:05 -0800

    abiut

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -96,19 +96,15 @@ function ContactForm() {
 function ContactDetails() {
   return (
     <FadeIn>
-      <h2 className="font-display text-base font-semibold text-neutral-950">
-        Our offices
-      </h2>
-      <p className="mt-6 text-base text-neutral-600">
-        Prefer doing things in person? We don’t but we have to list our
-        addresses here for legal reasons.
-      </p>
+      <h1 className="font-display text-lg font-semibold text-neutral-950">
+        I usually work from
+      </h1>
 
       <Offices className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2" />
 
-      <Border className="mt-16 pt-16">
+      {/* <Border className="mt-16 pt-16">
         <h2 className="font-display text-base font-semibold text-neutral-950">
-          Email us
+          Email me
         </h2>
         <dl className="mt-6 grid grid-cols-1 gap-8 text-sm sm:grid-cols-2">
           {[
@@ -128,11 +124,11 @@ function ContactDetails() {
             </div>
           ))}
         </dl>
-      </Border>
+      </Border> */}
 
       <Border className="mt-16 pt-16">
         <h2 className="font-display text-base font-semibold text-neutral-950">
-          Follow us
+          Follow me
         </h2>
         <SocialMedia className="mt-6" />
       </Border>
@@ -148,8 +144,8 @@ export const metadata: Metadata = {
 export default function Contact() {
   return (
     <>
-      <PageIntro eyebrow="Contact us" title="Let’s work together">
-        <p>We can’t wait to hear from you.</p>
+      <PageIntro eyebrow="Contact me" title="Let’s work together">
+        <p>I can’t wait to hear from you.</p>
       </PageIntro>
 
       <Container className="mt-24 sm:mt-32 lg:mt-40">
